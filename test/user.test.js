0b7ec8ab30f60348bd3acf45d2df9447cf8c547f
Edvaldo Santana <edypaulista152@gmail.com>
2020-12-17 16:55:06 -0300

    atualização

--- test/user.test.js
+++ test/user.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-test('Deve listar todos os usuários', ()=> {
+test('Deve listar 1 todos os usuários', ()=> {
     return request(app).get('/users')
         .then((res)=> {
             expect(res.status).toBe(200);
             expect(res.body).toHaveLength(1);
-            expect(res.body[0]).toHaveProperty('name','Edvaldo');
+            expect(res.body[0]).toHavePropertyw('name','Edvaldo');
 
     });
        
